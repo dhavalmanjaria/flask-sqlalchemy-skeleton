39241de342b1ec7f3dd386940d32e8042d0a1247
dhavalmanjaria <Dhaval Anjaria>
2021-08-23 00:52:52 +0530

    Update start script

--- src/wsgi.py
+++ src/wsgi.py
@@ -2,8 +2,8 @@ from data import init_db
 from app import app, create_app
 
 # These need to be called outside the main block
-create_app()
-init_db(app)
+wsgi_app = create_app()
+init_db(wsgi_app)
 
 if __name__ == "__main__":
 
